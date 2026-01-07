d7be169083e4751d3f667ef00b255cda2907f4e0
pedro <pedrofpteixeira4@gmail.com>
2026-01-07 19:55:14 -0300

    feat: Add environment configuration and refactor settings to use python-decouple for sensitive data

--- plataforma/settings.py
+++ plataforma/settings.py
@@ -16,6 +16,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config, Csv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,23 +38,23 @@ STATICFILES_STORAGE = (
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-atwv%yxwmtv*kny4up8c0jqxni6kr4)s-#+(a@_@u$*+torg=o'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1', cast=Csv())
 
 
 
 # Configuração de e-mail para reset de senha (ajuste para produção)
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'  # Altere para o servidor SMTP do seu provedor
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'seu_email@gmail.com'  # Altere para seu e-mail
-EMAIL_HOST_PASSWORD = 'sua_senha_de_aplicativo'  # Altere para sua senha de app ou senha do e-mail
-DEFAULT_FROM_EMAIL = 'noreply@atividades.com'
+EMAIL_BACKEND = config('EMAIL_BACKEND', default='django.core.mail.backends.smtp.EmailBackend')
+EMAIL_HOST = config('EMAIL_HOST', default='smtp.gmail.com')
+EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default='noreply@atividades.com')
 
 # Application definition
 

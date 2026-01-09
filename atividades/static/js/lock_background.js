991ec97be542a102708a8cf010868cb8a2c20477
pedro <pedrofpteixeira4@gmail.com>
2026-01-09 19:59:03 -0300

    feat: Enhance dashboard layout and functionality with improved HTMX integration, including modal actions and course information display

--- atividades/static/js/lock_background.js
+++ atividades/static/js/lock_background.js
@@ -7,8 +7,7 @@ document.body.addEventListener('htmx:afterSwap', (e) => {
 // Quando o modal for removido
 document.body.addEventListener('htmx:beforeSwap', (e) => {
   if (
-    e.target.id === 'modal-container' &&
-    e.detail.xhr.response === ''
+    (e.target.id === 'modal-container' && e.detail.xhr.response === '') || (e.target.id === 'aluno-container')
   ) {
     document.body.classList.remove('modal-open')
   }
